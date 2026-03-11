2a3ceed236e0bf72ed14a87d57fcaa80922ec219
vn_sunday <vnsunday@vnsunday.vn>
2026-03-11 20:42:50 +0700

    Constants

---
+++ src/apps/contants/contants.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Total, NET, T, I
+double az_constant25[13][4] = {
+	{ 0.0, 0.0, 0.0, 0.0 }, 
+	{  },
+
+};
+
+int main(int argc, char const *argv[])
+{
+	int iGross = 0;
+	int iNET = 1;
+	int iPIT = 2;
+	
+	return 0;
+}
