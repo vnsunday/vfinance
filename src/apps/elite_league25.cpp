3bc7be33c691056c8b4f104d83393f3039b402d4
vn_sunday <vnsunday@vnsunday.vn>
2026-04-20 22:19:35 +0700

    league

---
+++ src/apps/elite_league25.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int const iRevenue = 0;
+int const iOpIncome = 1;
+int const iAsset = 2;
+int const iEmp = 3; /* Count people */
+
+int azConstant25[][4] = {
+    { 0, 0, 0, 0 },  // CSG
+    { 287100, 128500, 619000, 228000 },  // MS 
+    {  67540,  10990, 151880, 264300 }, // I
+    {   8000,   415,  18000,  20400 }, // WD (day)
+    {  13400,  2000,  31200,  38300 }, // VMW (ware)
+    {  13280,  1824,  26040,  29187 }, // SVN (service)
+    {   2170,   121,   4700,   7348 }, // ZS (Scaler)
+    {   2920,   149,   9710,   6366 }, // OKT
+    {  41525,  8331, 112305,  83334 }, // SF (Sales)
+    {  57400, 17680, 106400, 149935 }, // ORCL
+    {   9220,  1240,   7820,  16068 }, // Palo-Alto
+    {  42400, 11957,  81743, 110650 }, // SAP 
+    {   3900,   766,   6320,   6578 }, // F5
+    {   2530,   173,   3283,   7800 }, // NNT
+    {   5170,   893,  13800,  21400 }  // OPT
+};
+
+int main(int argc, char const *argv[])
+{
+    /* Print in Table view */
+    return 0;
+}
