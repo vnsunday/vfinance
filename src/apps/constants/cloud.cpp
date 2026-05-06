66a8728180dd4ffb4fce221ee2042a53ca60da87
vn_sunday <vnsunday@vnsunday.vn>
2026-05-06 09:59:58 +0700

    cloud

---
+++ src/apps/constants/cloud.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+string azCloud[] = {
+};
+
+string azAudit[] = {
+    "Internal",
+    "External (type)",
+    "Conformity",
+    "Non conformity",
+    "Corrective action"
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
