8e7ae91c224ca78a74243662d3016c80bf7626a0
vn_sunday <truyenspidermanvn@gmail.com>
2026-07-31 16:46:40 +0700

    scheduling

--- vfinance-java/src/com/vfinancedaily26/july31.java
+++ vfinance-java/src/com/vfinancedaily26/july31.java
@@ -10,4 +10,12 @@ public class july31 {
 		
 		Task.Schedule(new ITask[] { t1, t2}, new int[] { 1, 1} );
 	}
+	
+	public void d3_14half_plus3() {
+		ITask t1 = Task.Construct("Video", "ztable");
+		ITask t2 = Task.Construct("CostFunction", "");
+		ITask t3 = Task.Construct("Finger", "Template-Time");
+		
+		Task.Schedule(new ITask[] { t1, t2, t3 }, new int[] { 1, 1, 1 });
+	}
 }
