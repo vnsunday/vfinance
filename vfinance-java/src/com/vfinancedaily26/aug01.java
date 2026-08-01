d0ed1fbf224a54988526f015d24d0d473bd876ae
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-01 10:16:52 +0700

    scheduling multiple

--- vfinance-java/src/com/vfinancedaily26/aug01.java
+++ vfinance-java/src/com/vfinancedaily26/aug01.java
@@ -6,6 +6,8 @@ import com.vfinance.Task;
 public class aug01 {
 	public void d3_8half_plus3() {
 		ITask t1 = Task.Construct("Clip", "ztable");
-		ITask t2 = Task.Construct("", "");
+		ITask t2 = Task.Construct("Entext", "Long");
+		
+		Task.Schedule(new ITask[] { t1, t2 }, new int[] { 1, 1} );
 	}
 }
