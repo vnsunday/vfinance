66b05002a92c827f4efb6f6c85592e61d042cccd
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-01 10:15:53 +0700

    scheduling

---
+++ vfinance-java/src/com/vfinancedaily26/aug01.java
@@ -0,0 +1,11 @@
+package com.vfinancedaily26;
+
+import com.vfinance.ITask;
+import com.vfinance.Task;
+
+public class aug01 {
+	public void d3_8half_plus3() {
+		ITask t1 = Task.Construct("Clip", "ztable");
+		ITask t2 = Task.Construct("", "");
+	}
+}
