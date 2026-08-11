2991de37d425a69ecea40038285a803bc33a0c36
vnsunday <truyenspidermanvn@gmail.com>
2026-08-11 08:21:49 +0700

    text loading

---
+++ vfinance-java/src/com/vfinancedaily26/aug11.java
@@ -0,0 +1,11 @@
+package com.vfinancedaily26;
+
+import com.vfinance.ITask;
+import com.vfinance.Task;
+
+public class aug11 {
+	public void d3_8plus3() {
+		ITask t1 = Task.Construct("load-text", null);
+		Task.Schedule(new ITask[] { t1 }, null);
+	}
+}
