af45d6eae503f0946f4fa2b9af671f59a94a48c6
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-13 09:27:19 +0700

    List3

---
+++ vfinance-java/src/com/vfinancedaily26/aug13.java
@@ -0,0 +1,13 @@
+package com.vfinancedaily26;
+
+import com.vfinance.ITask;
+import com.vfinance.Task;
+
+public class aug13 {
+	public void d3_9plus3( ) {
+		ITask t1 = Task.Construct("EN-text", "Load");
+		t1.Graph("List3", new String[][] {
+			{ "China", "Micro. Circuit" }
+		});
+	}
+}
