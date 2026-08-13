f273877045e5625594525d65bdb518ff91d00072
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-13 20:48:14 +0700

    power zen

--- vfinance-java/src/com/vfinancedaily26/aug13.java
+++ vfinance-java/src/com/vfinancedaily26/aug13.java
@@ -10,4 +10,12 @@ public class aug13 {
 			{ "China", "Micro. Circuit" }
 		});
 	}
+	
+	public void d3_20plus3() {
+		ITask t1 = Task.Construct("Power", "Zen. Primitive");
+		ITask t2 = Task.Construct("En-text", "Load");
+		
+		t2.Graph("Key-Orientation-Clock", new String[][] {});
+		Task.Schedule(new ITask[] { t1, t2 }, null);
+	}
 }
