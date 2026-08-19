50ba5dd60151d5723ab0095f402fbf26703214e1
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-19 11:41:35 +0700

    book cover

---
+++ vfinance-java/src/com/vfinancedaily26/aug19.java
@@ -0,0 +1,15 @@
+package com.vfinancedaily26;
+
+public class aug19 {
+	public void d3_10_plus3() {
+		double ratio = 356 * 3.0 / 5.0;
+		String[][] azSchedule = {
+			{ "ZEN", "Power" }
+		};
+		
+		String[][] azBookCover = {
+			{ "Paper", "A4" },
+			{ "one-fold", "23" }
+		};
+	}
+}
